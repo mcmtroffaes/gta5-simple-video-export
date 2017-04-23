0cd8ffc932514db364a92b969b4dd0db5b6682c2
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-04-23 17:46:18 +0100

    Some more logger updates.

--- gta5-simple-video-export/dllmain.cpp
+++ gta5-simple-video-export/dllmain.cpp
@@ -3,13 +3,32 @@
 #include <mfplay.h>
 #include <mfreadwrite.h>
 
+#pragma comment(lib, "mfreadwrite.lib")
+
 #define SCRIPT_NAME "simple-video-export"
 #define SCRIPT_FOLDER "SVE"
 
-std::shared_ptr<spdlog::logger> logger;
-std::shared_ptr<PLH::IATHook> sinkwriter_hook;
+std::shared_ptr<spdlog::logger> logger = nullptr;
+std::shared_ptr<PLH::IATHook> sinkwriter_hook = nullptr;
 
-#pragma comment(lib, "mfreadwrite.lib")
+#define LOG_INFO(msg) logger->info("{}: {}", __func__, msg)
+#define LOG_DEBUG(msg) logger->debug("{}: {}", __func__, msg)
+#define LOG_ERROR(msg) logger->error("{}: {}", __func__, msg)
+#define LOG_TRACE(msg) logger->trace("{}: {}", __func__, msg)
+#define LOG_ENTER LOG_TRACE("enter")
+#define LOG_EXIT LOG_TRACE("exit")
+
+std::string ws2s(const std::wstring & s)
+{
+	int len;
+	size_t slength = s.length() + 1;
+	len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0);
+	char* buf = new char[len];
+	WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, buf, len, 0, 0);
+	std::string r(buf);
+	delete[] buf;
+	return r;
+}
 
 typedef HRESULT(__stdcall *tMFCreateSinkWriterFromURL)(
 	LPCWSTR       pwszOutputURL,
@@ -26,22 +45,27 @@ HRESULT __stdcall SinkWriterNew(
 	IMFSinkWriter **ppSinkWriter
 )
 {
-	logger->trace("SinkWriterNew: enter");
+	LOG_ENTER;
+	LOG_DEBUG("pwszOutputURL = " + ws2s(pwszOutputURL));
 	auto res = sinkwriter_orig_func(pwszOutputURL, pByteStream, pAttributes, ppSinkWriter);
-	logger->trace("SinkWriterNew: exit");
+	LOG_EXIT;
 	return res;
 }
 
 std::shared_ptr<PLH::IATHook> getHookMFCreateSinkWriterFromURL()
 {
+	LOG_ENTER;
 	std::shared_ptr<PLH::IATHook> hook(new PLH::IATHook);
 	hook->SetupHook("mfreadwrite.dll", "MFCreateSinkWriterFromURL", (BYTE*)&SinkWriterNew);
 	if (hook->Hook()) {
 		sinkwriter_orig_func = hook->GetOriginal<tMFCreateSinkWriterFromURL>();
+		LOG_DEBUG("MFCreateSinkWriterFromURL hooked");
+		LOG_EXIT;
 		return hook;
 	}
 	else {
-		logger->error("Failed to hook into MFCreateSinkWriterFromURL");
+		LOG_ERROR("MFCreateSinkWriterFromURL hook failed");
+		LOG_EXIT;
 		return nullptr;
 	}
 }
@@ -51,12 +75,31 @@ BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
 	switch (reason)
 	{
 	case DLL_PROCESS_ATTACH:
+		/* reset all variables */
+		logger = nullptr;
+		sinkwriter_hook = nullptr;
+		/* set up logger */
 		logger = spdlog::basic_logger_st(SCRIPT_NAME, SCRIPT_FOLDER "\\log.txt");
+		if (!logger) break;
+		logger->set_level(spdlog::level::trace);
+		logger->flush_on(spdlog::level::trace);
+		LOG_ENTER;
+		/* set up hooks */
 		sinkwriter_hook = getHookMFCreateSinkWriterFromURL();
 		break;
 	case DLL_PROCESS_DETACH:
-		sinkwriter_hook->UnHook();
-		spdlog::drop(SCRIPT_NAME);
+		/* clean up hooks */
+		if (sinkwriter_hook) {
+			sinkwriter_hook->UnHook();
+			sinkwriter_hook = nullptr;
+			LOG_DEBUG("MFCreateSinkWriterFromURL unhooked");
+		}
+		/* clean up logger */
+		if (logger) {
+			LOG_EXIT;
+			logger = nullptr;
+			spdlog::drop(SCRIPT_NAME);
+		}
 		break;
 	}		
 	return TRUE;
