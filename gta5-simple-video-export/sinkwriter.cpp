c123bea0a834ca6dba7c1ad6f2301f09486c4a84
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-04-25 17:30:20 +0100

    Simplify code: don't store original function pointer separately.

--- gta5-simple-video-export/sinkwriter.cpp
+++ gta5-simple-video-export/sinkwriter.cpp
@@ -7,60 +7,67 @@
 
 #pragma comment(lib, "mfreadwrite.lib")
 
+template <class FuncType>
+std::unique_ptr<PLH::IATHook> CreateIATHook(const char *library_name, const char *func_name, FuncType new_func) {
+	LOG_ENTER;
+	std::unique_ptr<PLH::IATHook> hook(new PLH::IATHook());
+	hook->SetupHook(library_name, func_name, (BYTE*)new_func);
+	if (hook->Hook()) {
+		logger->debug("hook set for {}", func_name);
+	}
+	else {
+		hook = nullptr;
+		logger->debug("failed to hook {}", func_name);
+	}
+	return hook;
+	LOG_EXIT;
+}
+
 /* helper function to create a hook for a virtual function */
 template <class ClassType, class FuncType>
-std::unique_ptr<PLH::VFuncDetour> CreateVFuncDetour(ClassType *p_instance, int vfunc_index, LPVOID new_func, FuncType *orig_func) {
+std::unique_ptr<PLH::VFuncDetour> CreateVFuncDetour(ClassType *p_instance, int vfunc_index, FuncType new_func) {
 	LOG_ENTER;
 	std::unique_ptr<PLH::VFuncDetour> hook(new PLH::VFuncDetour());
 	if (!p_instance) {
 		hook = nullptr;
-		*orig_func = nullptr;
-		LOG_ERROR("hook failed (instance is null)");
+		LOG_ERROR("hook failed: class instance is null");
 	}
 	else {
 		hook->SetupHook(*(BYTE***)p_instance, vfunc_index, (BYTE*)new_func);
 		if (hook->Hook()) {
-			*orig_func = hook->GetOriginal<FuncType>();
 			LOG_DEBUG("hook set");
 		}
 		else {
 			hook = nullptr;
-			*orig_func = nullptr;
-			LOG_ERROR("hook failed (polyhook error)");
+			LOG_ERROR("hook failed");
 		}
 	}
 	LOG_EXIT;
 	return hook;
 }
 
-typedef HRESULT (__stdcall *FinalizeFuncType)(
-	IMFSinkWriter *pThis
-	);
 
 std::unique_ptr<PLH::IATHook> sinkwriter_hook = nullptr;
 std::unique_ptr<PLH::VFuncDetour> finalize_hook = nullptr;
-decltype(&MFCreateSinkWriterFromURL) sinkwriter_orig_func = nullptr;
-FinalizeFuncType finalize_orig_func = nullptr;
 
 HRESULT __stdcall FinalizeNew(
 	IMFSinkWriter *pThis
 	) {
 	LOG_ENTER;
-	if (!finalize_orig_func) {
-		LOG_ERROR("finalize_orig_func not initialized; hook not set up?");
+	if (!finalize_hook) {
+		LOG_ERROR("finalize_hook not set up");
 		return E_FAIL;
 	}
-	logger->trace("finalize_orig_func: enter");
-	auto hr = finalize_orig_func(pThis);
-	logger->trace("finalize_orig_func: exit");
+	logger->trace("IMFSinkWriter::Finalize: enter");
+	auto hr = finalize_hook->GetOriginal<decltype(&FinalizeNew)>()(pThis);
+	logger->trace("IMFSinkWriter::Finalize: exit");
 	/* we will no longer use this IMFSinkWriter instance, so clean up all hooks into it */
 	finalize_hook = nullptr;
-	finalize_orig_func = nullptr;
+	sinkwriter_hook = nullptr;
 	logger->debug("cleared all IMFSinkWriter hooks");
 	LOG_EXIT;
 	return hr;
 }
-
 
 HRESULT __stdcall SinkWriterNew(
 	LPCWSTR       pwszOutputURL,
@@ -70,16 +77,16 @@ HRESULT __stdcall SinkWriterNew(
 	)
 {
 	LOG_ENTER;
-	if (!sinkwriter_orig_func) {
+	if (!sinkwriter_hook) {
 		LOG_ERROR("sinkwriter_orig_func not initialized; hook not set up?");
 		return E_FAIL;
 	}
-	logger->trace("sinkwriter_orig_func: enter");
-	auto hr = sinkwriter_orig_func(pwszOutputURL, pByteStream, pAttributes, ppSinkWriter);
-	logger->trace("sinkwriter_orig_func: exit");
+	logger->trace("MFCreateSinkWriterFromURL: enter");
+	auto hr = sinkwriter_hook->GetOriginal<decltype(&SinkWriterNew)>()(pwszOutputURL, pByteStream, pAttributes, ppSinkWriter);
+	logger->trace("MFCreateSinkWriterFromURL: exit");
 	if (SUCCEEDED(hr)) {
 		LOG_TRACE("hooking IMFSinkWriter::Finalize");
-		finalize_hook = CreateVFuncDetour(*ppSinkWriter, 11, &FinalizeNew, &finalize_orig_func);
+		finalize_hook = CreateVFuncDetour(*ppSinkWriter, 11, &FinalizeNew);
 	}
 	LOG_EXIT;
 	return hr;
@@ -88,16 +95,7 @@ HRESULT __stdcall SinkWriterNew(
 void Hook()
 {
 	LOG_ENTER;
-	sinkwriter_hook.reset(new PLH::IATHook);
-	sinkwriter_hook->SetupHook("mfreadwrite.dll", "MFCreateSinkWriterFromURL", (BYTE*)&SinkWriterNew);
-	if (sinkwriter_hook->Hook()) {
-		sinkwriter_orig_func = sinkwriter_hook->GetOriginal<decltype(&MFCreateSinkWriterFromURL)>();
-		LOG_DEBUG("MFCreateSinkWriterFromURL hooked");
-	}
-	else {
-		sinkwriter_hook = nullptr;
-		LOG_ERROR("MFCreateSinkWriterFromURL hook failed");
-	}
+	sinkwriter_hook = CreateIATHook("mfreadwrite.dll", "MFCreateSinkWriterFromURL", &SinkWriterNew);
 	LOG_EXIT;
 }
 
@@ -106,7 +104,5 @@ void Unhook()
 	LOG_ENTER;
 	sinkwriter_hook = nullptr;
 	finalize_hook = nullptr;
-	sinkwriter_orig_func = nullptr;
-	finalize_orig_func = nullptr;
 	LOG_EXIT;
 }
