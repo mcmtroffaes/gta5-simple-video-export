04f769f8dffef891107ca120f8a68f1c0c3f9ffd
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-04-28 18:21:00 +0100

    Get video and audio info, and boilerplate code to write audio and video samples.

--- gta5-simple-video-export/sinkwriter.cpp
+++ gta5-simple-video-export/sinkwriter.cpp
@@ -11,12 +11,20 @@
 #pragma comment(lib, "mfreadwrite.lib")
 #pragma comment(lib, "mfuuid.lib")
 
+struct AudioInfo {
+	DWORD stream_index;
+};
+
+struct VideoInfo {
+	DWORD stream_index;
+};
+
 std::unique_ptr<PLH::IATHook> sinkwriter_hook = nullptr;
 std::unique_ptr<PLH::VFuncDetour> setinputmediatype_hook = nullptr;
 std::unique_ptr<PLH::VFuncDetour> writesample_hook = nullptr;
 std::unique_ptr<PLH::VFuncDetour> finalize_hook = nullptr;
-int stream_index_audio = -1;
-int stream_index_video = -1;
+std::unique_ptr<AudioInfo> audio_info = nullptr;
+std::unique_ptr<VideoInfo> video_info = nullptr;
 
 void UnhookVFuncDetours()
 {
@@ -24,8 +32,8 @@ void UnhookVFuncDetours()
 	setinputmediatype_hook = nullptr;
 	writesample_hook = nullptr;
 	finalize_hook = nullptr;
-	stream_index_audio = -1;
-	stream_index_video = -1;
+	audio_info = nullptr;
+	video_info = nullptr;
 	LOG_EXIT;
 }
 
@@ -37,6 +45,24 @@ void Unhook()
 	LOG_EXIT;
 }
 
+std::unique_ptr<AudioInfo> GetAudioMediaType(DWORD stream_index, const IMFMediaType & input_media_type) {
+	LOG_ENTER;
+	std::unique_ptr<AudioInfo> info(new AudioInfo);
+	logger->debug("audio stream index = {}", stream_index);
+	info->stream_index = stream_index;
+	LOG_EXIT;
+	return info;
+}
+
+std::unique_ptr<VideoInfo> GetVideoMediaType(DWORD stream_index, const IMFMediaType & input_media_type) {
+	LOG_ENTER;
+	std::unique_ptr<VideoInfo> info(new VideoInfo);
+	logger->debug("video stream index = {}", stream_index);
+	info->stream_index = stream_index;
+	LOG_EXIT;
+	return info;
+}
+
 STDAPI SinkWriterSetInputMediaType(
 	IMFSinkWriter *pThis,
 	DWORD         dwStreamIndex,
@@ -46,14 +72,41 @@ STDAPI SinkWriterSetInputMediaType(
 	LOG_ENTER;
 	if (!setinputmediatype_hook) {
 		logger->error("IMFSinkWriter::SetInputMediaType hook not set up");
+		LOG_EXIT;
 		return E_FAIL;
 	}
 	auto original_func = setinputmediatype_hook->GetOriginal<decltype(&SinkWriterSetInputMediaType)>();
 	logger->trace("IMFSinkWriter::SetInputMediaType: enter");
 	auto hr = original_func(pThis, dwStreamIndex, pInputMediaType, pEncodingParameters);
 	logger->trace("IMFSinkWriter::SetInputMediaType: exit {}", hr);
+	if (SUCCEEDED(hr)) {
+		GUID major_type = { 0 };
+		auto hr2 = pInputMediaType->GetMajorType(&major_type);
+		if (FAILED(hr2)) {
+			logger->error("failed to get major type for stream at index {}", dwStreamIndex);
+		}
+		else if (major_type == MFMediaType_Audio) {
+			audio_info = GetAudioMediaType(dwStreamIndex, *pInputMediaType);
+		}
+		else if (major_type == MFMediaType_Video) {
+			video_info = GetVideoMediaType(dwStreamIndex, *pInputMediaType);
+		}
+		else {
+			logger->debug("unknown stream at index {}", dwStreamIndex);
+		}
+	}
 	LOG_EXIT;
 	return hr;
+}
+
+void WriteAudioSample(const IMFSample & sample) {
+	LOG_ENTER;
+	LOG_EXIT;
+}
+
+void WriteVideoSample(const IMFSample & sample) {
+	LOG_ENTER;
+	LOG_EXIT;
 }
 
 STDAPI SinkWriterWriteSample(
@@ -70,6 +123,16 @@ STDAPI SinkWriterWriteSample(
 	logger->trace("IMFSinkWriter::WriteSample: enter");
 	auto hr = original_func(pThis, dwStreamIndex, pSample);
 	logger->trace("IMFSinkWriter::WriteSample: exit {}", hr);
+	if (audio_info) {
+		if (dwStreamIndex == audio_info->stream_index) {
+			WriteAudioSample(*pSample);
+		}
+	}
+	if (video_info) {
+		if (dwStreamIndex == video_info->stream_index) {
+			WriteVideoSample(*pSample);
+		}
+	}
 	LOG_EXIT;
 	return hr;
 }
