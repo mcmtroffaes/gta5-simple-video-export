2946d47effbbf42f3e55c0123c82b6487c802b79
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-11 09:35:13 +0100

    Target Visual Studio 2019.

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "..\spdlog\include\spdlog\sinks\stdout_sinks.h"
+#include "..\spdlog\include\spdlog\sinks\stdout_color_sinks.h"
 #include "..\gta5-simple-video-export\settings.h"
 
 std::shared_ptr<spdlog::logger> logger = nullptr;
@@ -8,7 +8,7 @@ std::unique_ptr<Settings> settings = nullptr;
 int main()
 {
 	/* load settings without logger, to get log_level and log_flush_on */
-	logger = spdlog::rotating_logger_mt;
+	logger = spdlog::stdout_color_mt(SCRIPT_NAME);
 	settings.reset(new Settings);
 	/* set up logger */
 	settings->ResetLogger();
