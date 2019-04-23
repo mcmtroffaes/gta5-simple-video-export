1377291e1a5b491e2778c3780cece2a3c67a788c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-23 11:29:27 +0100

    Use strnlen instead of strnlen_s for compatibility with other compilers.

--- common/logger.cpp
+++ common/logger.cpp
@@ -100,7 +100,7 @@ void AVLogCallback(void* avcl, int level, const char* fmt, va_list vl)
 		}
 	}
 	// only write log message on newline
-	size_t i = strnlen_s(fmt, sizeof(line));
+	size_t i = strnlen(fmt, sizeof(line));
 	if ((i > 0) && (fmt[i - 1] == '\n')) {
 		// remove newline (spdlog adds a newline automatically)
 		if ((pos > 0) && (line[pos - 1] == '\n')) {
